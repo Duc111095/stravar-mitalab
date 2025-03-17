f6f0fa04e3ea2111aa01a7f3165e1d50c0265789
Duc111095 <101342718+Duc111095@users.noreply.github.com>
2025-03-17 21:25:00 +0700

    add utils class

--- src/main/java/com/ducnh/strava_mitalab/utils/PagingUtils.java
+++ src/main/java/com/ducnh/strava_mitalab/utils/PagingUtils.java
@@ -32,7 +32,59 @@ public class PagingUtils {
 			final int firstElement = ((inputPaging.getPage().intValue() - 1) * inputPaging.getPageSize().intValue()) + inputPaging.getIgnoreFirstN() + 1;
 			
 			// If the last element fits in one page, return one page
+			if (lastElement <= StravaConfig.MAX_PAGE_SIZE.intValue()) {
+				stravaPaging.add(new Paging(Integer.valueOf(1), Integer.valueOf(lastElement), inputPaging.getIgnoreFirstN(), 0));
+				return stravaPaging;
+			}
+			
+			// Otherwise, return a series of pages
+			int currentPage = 0;
+			for (int i = 1; i <= lastElement; i = i + StravaConfig.MAX_PAGE_SIZE.intValue()) {
+				currentPage++;
+				if (((currentPage * StravaConfig.MAX_PAGE_SIZE.intValue())) >= firstElement) {
+					final int ignoreLastN = Math.max(0, (currentPage * StravaConfig.MAX_PAGE_SIZE.intValue()) - 1);
+					final int ignoreFirstN = Math.max(0,  firstElement - ((currentPage - 1) * StravaConfig.MAX_PAGE_SIZE.intValue()) - 1);
+					stravaPaging.add(new Paging(Integer.valueOf(currentPage), StravaConfig.MAX_PAGE_SIZE, ignoreFirstN, ignoreLastN));
+				}
+			}
+			return stravaPaging;
 		}
+		
+		public static <T> List<T> ignoreFirstN(final List<T> list, final int ignoreFirstN) {
+			if (ignoreFirstN < 0) {
+				throw new IllegalArgumentException(Messages.string("PagingUtils.cannotRemove") + ignoreFirstN + Messages.string("PagingUtils.itemsFromAList"));
+			}
+			if (list == null) {
+				return null;
+			}
+			if (ignoreFirstN == 0) {
+				return list;
+			}
+			if (ignoreFirstN >= list.size()) {
+				return new ArrayList<T>();
+			}
+			
+			// return list.subList(ignoreFirstN, list.size() - 1);
+			final ArrayList<T> returnList = new ArrayList<T>();
+			for (int i = ignoreFirstN; i < list.size(); i++) {
+				returnList.add(list.get(i));
+			}
+			return returnList;
+		}
+		
+		public static <T> List<T> ignoreLastN(final List<T> list, final int ignoreLastN) {
+			if (ignoreLastN < 0) {
+				throw new IllegalArgumentException(Messages.string("PagingUtils.cannotRemove") + ignoreLastN + Messages.string("PagingUtils.itemsFromAList"));
+			}
+			if (list == null) {
+				return null;
+			}
+			if (ignoreLastN == 0) {
+				return list;
+			}
+			return list.subList(0, list.size() - ignoreLastN);
+		}
+		
 		public static void validatePagingArgument(final Paging pagingInstruction) {
 			if (pagingInstruction == null) {
 				return;
@@ -46,7 +98,7 @@ public class PagingUtils {
 			if((pagingInstruction.getIgnoreLastN() > 0) && (pagingInstruction.getIgnoreLastN() > pagingInstruction.getPageSize().intValue())) {
 				throw new IllegalArgumentException(Messages.string("PagingUtils.IgnoreTooHigh"));
 			}
-			if((pagingInstruction.getIgnoreFirstN() > 0) && (pagingInstruction.getIgnoreFirstN() > pagingInstruction.getPageSize().intValue)) {
+			if((pagingInstruction.getIgnoreFirstN() > 0) && (pagingInstruction.getIgnoreFirstN() > pagingInstruction.getPageSize().intValue())) {
 				throw new IllegalArgumentException(Messages.string("PagingUtils.IgnoreTooHigh"));
 			}
 		}
